dfc3d6cbb56e344d806ce24ec99a5851f0c3e20d
Matthew Parry <34604130+parry-do@users.noreply.github.com>
2021-12-12 14:31:14 +0000

    Sets configuration to include docker installation

--- scripts/configure.sh
+++ scripts/configure.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 echo "Updating apt"
 apt -qq -y update 2> /dev/null > /dev/null
-echo "Installing python3"
-apt -qq -y install python3 2> /dev/null > /dev/null
+
 echo "Upgrading system (this will take a while)"
 apt -qq -y upgrade 2> /dev/null > /dev/null
 
+echo "Installing python3 if not already installed"
+apt -qq -y install python3 2> /dev/null > /dev/null
+
+echo "Installing docker"
+apt -qq -y install docker.io 2> /dev/null > /dev/null
+apt -qq -y install docker-compose 2> /dev/null > /dev/null
+
+echo "Building docker files and docker images"
 python3 configure.py
