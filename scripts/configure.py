d25b0c6e9a1acf6bb69f1838e2662da3aab6eccf
Matthew Parry <34604130+parry-do@users.noreply.github.com>
2021-12-31 16:41:30 +0000

    Removes separate mondodb dockerfile

--- scripts/configure.py
+++ scripts/configure.py
@@ -44,7 +44,7 @@ options.update({
 })
 
 # Docker files are created with correct options
-for path in ['app', 'nginx', 'db']:
+for path in ['app', 'nginx']:
     name = f'{path}.Dockerfile'
     with open(join(BASE_DIR,'scripts',name),'r') as f:
         with open(join(
--- scripts/db.Dockerfile
+++
@@ -1,7 +0,0 @@
-FROM mongo:4.0.8
-
-LABEL MAINTAINER="{FIRST_NAME} {LAST_NAME} <{EMAIL}>"
-
-ADD ./mongo-init.js /mongo-init.js
-
-CMD ["mongod", "--auth"]
